64062083b38f7d6585ceaa27ec21554b5492ce45
Joao Estrela Leite <4estrj82@solent.ac.uk>
2020-01-22 10:44:38 +0000

    week 2 finished

--- AAA_MyClassWork/ReportSystem/app/src/main/java/com/example/reportsystem/MainActivity.java
+++ AAA_MyClassWork/ReportSystem/app/src/main/java/com/example/reportsystem/MainActivity.java
@@ -1,33 +1,27 @@
-package com.example.feettometresactivity;
+package com.example.reportsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.EditText;
-
-import org.w3c.dom.Text;
-
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b = (Button)findViewById(R.id.btn1);
-        b.setOnClickListener(this);
+
 
 
     }
-
     public void onClick(View view)
     {
-        TextView tv = (TextView)findViewById(R.id.tv1);
-        EditText et = (EditText)findViewById(R.id.et1);
-        double feet = Double.parseDouble(et.getText().toString());
-        double meters = feet*0.305;
-        tv.setText("In meters that is: " + meters);
+
     }
+
+
+
 }
