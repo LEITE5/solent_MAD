300802704e12b474684854dd8b5b1124a7f0dce0
unknown <leitesdzn@gmail.com>
2020-01-28 17:27:03 +0000

    week 3 map fixed with lon and lat EditText

--- AAA_MyClassWork/week3/app/src/main/java/com/example/mapping/MainActivity.java
+++ AAA_MyClassWork/week3/app/src/main/java/com/example/mapping/MainActivity.java
@@ -16,30 +16,38 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     MapView mv;
+    double lat = 50.8982352;
+    double lon = -1.400974;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-        Button b = (Button) findViewById(R.id.btn);
-        b.setOnClickListener(this);
         setContentView(R.layout.activity_main);
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+
 
         mv = findViewById(R.id.map1);
 
         mv.setMultiTouchControls(true);
         mv.getController().setZoom(16.0);
-        mv.getController().setCenter(new GeoPoint(50.8982352, -1.400974));
+        mv.getController().setCenter(new GeoPoint(lat, lon));
 
 
     }
 
     @Override
-    public void onClick(View v) {
-
+    public void onClick(View view) {
+        EditText et1 = (EditText)findViewById(R.id.et1);
+        EditText et2 = (EditText)findViewById(R.id.et2);
+        double lat = Double.parseDouble(et1.getText().toString());
+        double lon = Double.parseDouble(et2.getText().toString());
+        mv.getController().setCenter(new GeoPoint(lat, lon));
     }
 }
